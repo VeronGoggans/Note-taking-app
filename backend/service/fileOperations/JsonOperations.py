52ff67d9fe8008ab84cdf8a44c9ee53f96f622bf
VeronGoggans <j.s.v.goggans@gmail.com>
2023-11-14 01:15:08 +0100

    added propper documentation

--- backend/service/fileOperations/JsonOperations.py
+++ backend/service/fileOperations/JsonOperations.py
@@ -4,19 +4,34 @@ class Json:
     def __init__(self) -> None:
          pass
 
-    # This method loads a json file from a given file path.
-    # This method returns the json data that is in the json file.
+    
     @staticmethod
-    def load_json_file(file_path):
+    def load_json_file(file_path) -> dict:
+        """
+        Load JSON data from a file.
+
+        Args:
+            file_path (str): The path of the JSON file to load.
+
+        Returns:
+            dict: The loaded JSON data as a dictionary.
+        """
         with open(file_path, 'r') as file:
             data = json.load(file)
             return data
     
 
-    # This method updates a json file with new/updated data.
-    # This method has no return value.
     @staticmethod
-    def update_json_file(file_path, updated_data):
+    def update_json_file(file_path, updated_data) -> None:
+        """
+        Update a JSON file with new data.
+
+        Args:
+            file_path (str): The path of the JSON file to update.
+            updated_data (dict): The updated data to write to the file.
+
+        Returns:
+            None: This method does not return a value.
+        """
         with open(file_path, 'w') as file:
             json.dump(updated_data, file, indent=4)
-            file.close()
