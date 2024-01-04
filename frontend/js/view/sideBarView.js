afec28cd67877ea0475a7934508af152f1a40592
VeronGoggans <j.s.v.goggans@gmail.com>
2024-01-04 16:07:27 +0100

    made some styling changes to the note button inside the editor.

--- frontend/js/view/sideBarView.js
+++ frontend/js/view/sideBarView.js
@@ -4,16 +4,24 @@ class SidebarView {
         this._sidebar = document.querySelector('.sidebar');
         this._wrapper = document.querySelector('.wrapper');
         this._createNoteButton = document.querySelector('.create-note-btn');
+        this._createSNoteSpan = this._createNoteButton.querySelector('span');
         this._backButton = document.querySelector('.exit-folder-btn');
+        this._backSpan = this._backButton.querySelector('span');
         this._homeButton = document.querySelector('.home-screen-btn');
+        this._homeSpan = this._homeButton.querySelector('span');
         this._collapsed = false;
     }
 
     collapseButtons() {
-        console.log('collapse buttons');
-        this._createNoteButton.textContent = '';
-        this._backButton.textContent = '';
-        this._homeButton.textContent = '';
+        this._createSNoteSpan.textContent = '';
+        this._backSpan.textContent = '';
+        this._homeSpan.textContent = '';
+    }
+
+    openButtons() {
+        this._createSNoteSpan.textContent = 'Note';
+        this._backSpan.textContent = 'Back';
+        this._homeSpan.textContent = 'Home';
     }
 
 
@@ -21,8 +29,9 @@ class SidebarView {
         if (window.innerWidth < 700) {
             this._collapsed = true;
             this.collapseButtons();
-        } else if (window.innerWidth >= 730 && this._collapsed === true) {
+        } else if (window.innerWidth >= 700 && this._collapsed === true) {
             this._collapsed = false;
+            this.openButtons();
         }
     }
 }
