5a572f853299fdff3f73297fd4d1f4d692008027
VeronGoggans <j.s.v.goggans@gmail.com>
2024-04-13 17:24:51 +0200

    also fixed the settingsModel

--- src/frontend/js/model/settingModel.js
+++ src/frontend/js/model/settingModel.js
@@ -1,58 +1,31 @@
+import { RequestOptionsBuilder } from "../util/builders/requestOptionsBuilder.js";
+
 export class SettingModel {
 
     async getTheme(endpoint) {
-        try {
-            const RESPONSE = await fetch(`${endpoint}`);
-            if (!RESPONSE.ok) throw new Error(`HTTP error Status: ${RESPONSE.status}`);
-            return await RESPONSE.json();
-        } catch(error) {
-            console.error('Error fetching data: ', error.message);
-            throw error;
-        }
+        const OPTIONS = RequestOptionsBuilder.buildGetOptions();
+        return this.#fetchData(endpoint, OPTIONS)
     }
 
     async updateTheme(endpoint, theme) {
-        const PUT_THEME_OBJECT = {
-            'theme': theme
-        }
-        const OPTIONS = {
-            method: 'PUT',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(PUT_THEME_OBJECT)
-        }
-        try {
-            const RESPONSE = await fetch(`${endpoint}`, OPTIONS);
-            if (!RESPONSE.ok) throw new Error(`HTTP error Status: ${RESPONSE.status}`);
-            return await RESPONSE.json();
-        } catch (error) {
-            console.error('Error fetching data: ', error.message);
-            throw error;
-        }
+        const OPTIONS = RequestOptionsBuilder.buildPutOptions({'theme': theme});
+        return this.#fetchData(endpoint, OPTIONS)
     }
 
     async getEditorPageStyle(endpoint) {
-        try {
-            const RESPONSE = await fetch(`${endpoint}`);
-            if (!RESPONSE.ok) throw new Error(`HTTP error Status: ${RESPONSE.status}`);
-            return await RESPONSE.json();
-        } catch(error) {
-            console.error('Error fetching data: ', error.message);
-            throw error;
-        }
+        const OPTIONS = RequestOptionsBuilder.buildGetOptions();
+        return this.#fetchData(endpoint, OPTIONS)
     }
 
     async updateEditorPageStyle(endpoint, style) {
-        const PUT_EDITOR_PAGE_STYLE_OBJECT = {
-            'style': style
-        }
-        const OPTIONS = {
-            method: 'PUT',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(PUT_EDITOR_PAGE_STYLE_OBJECT)
-        }
+        const OPTIONS = RequestOptionsBuilder.buildPutOptions({'style': style});
+        this.#fetchData(endpoint, OPTIONS)
+    }
+
+    async #fetchData(endpoint, options) {
         try {
-            const RESPONSE = await fetch(`${endpoint}`, OPTIONS);
-            if (!RESPONSE.ok) throw new Error(`HTTP error Status: ${RESPONSE.status}`);
+            const RESPONSE = await fetch(`${endpoint}`, options);
+            if (!RESPONSE.ok) throw new Error(`HTTP error Status: ${RESPONSE.status}`)
             return await RESPONSE.json();
         } catch (error) {
             console.error('Error fetching data: ', error.message);
