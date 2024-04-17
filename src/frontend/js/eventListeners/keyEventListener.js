08fc4c0584d43d3894a007191c34be25ad3eb09a
VeronGoggans <j.s.v.goggans@gmail.com>
2024-04-17 18:09:12 +0200

    added a key binding to exit the editor Cntrl + e

--- src/frontend/js/eventListeners/keyEventListener.js
+++ src/frontend/js/eventListeners/keyEventListener.js
@@ -2,13 +2,17 @@ export class KeyEventListener {
     constructor(view) {
         this.view = view; 
         this.editorWrapper = document.querySelector('.editor-paper');
+        this.eKeyCode = 69;
+        this.nKeyCode = 78;
+        this.sKeyCode = 83;
         this.saveKeyEvent();
         this.newKeyEvent();
+        this.exitEditorKeyEvent();
     }
 
     saveKeyEvent() {
         this.editorWrapper.addEventListener("keydown", (event) => {
-            if (event.ctrlKey && (event.key === "s" || event.keyCode === 83)) {
+            if (event.ctrlKey && (event.key === "s" || event.keyCode === this.sKeyCode)) {
                 // Prevent the default behavior (e.g., browser saving the page)
                 event.preventDefault();
 
@@ -22,11 +26,20 @@ export class KeyEventListener {
 
     newKeyEvent() {
         this.editorWrapper.addEventListener("keydown", (event) => {
-            if (event.ctrlKey && (event.key === "n" || event.keyCode === 83)) {
-                // Prevent the default behavior (e.g., browser saving the page)
+            if (event.ctrlKey && (event.key === "n" || event.keyCode === this.nKeyCode)) {
                 event.preventDefault();
                 
                 this.view.new();
+            }
+        })
+    }
+
+    exitEditorKeyEvent() {
+        this.editorWrapper.addEventListener("keydown", (event) => {
+            if (event.ctrlKey && (event.key === "e" || event.keyCode === this.eKeyCode)) {
+                event.preventDefault();
+
+                this.view.closeEditor();
             }
         })
     }
